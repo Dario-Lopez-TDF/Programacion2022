af7943dd40288f1925f9db344f67d27f670dbd04
Dario-Lopez-TDF <104174213+Dario-Lopez-TDF@users.noreply.github.com>
2022-04-23 13:33:48 -0300

    segundo

--- src/ejercicio1.ts
+++ src/ejercicio1.ts
@@ -1,8 +1,10 @@
+//Ejercicio: cálculo de descuento..
 let numero1: number = Number(prompt("ingrese el precio"));
-let numero2: number = Number(prompt("ingrese el % de descuento"));
-
+//Precio inicial del producto
+let numero2: number = 0.1;
+//Descuento del 10%
 let descuento: number = numero1 * numero2;
-
+//esto es el importe del descuento
 let resultado: number = numero1 - descuento;
-
-console.log("el resultado es: ", resultado);
+//...muestra el precio final del producto...
+console.log("el precio final es: ", resultado);
