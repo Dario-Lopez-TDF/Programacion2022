bf0ee1a9066d10b03a1368598345d2ada2e48f0d
Dario-Lopez-TDF <104174213+Dario-Lopez-TDF@users.noreply.github.com>
2022-04-24 18:03:46 -0300

    Segundo Ejercicio

--- src/ejercicio2.ts
+++ src/ejercicio2.ts
@@ -1,1 +1,16 @@
-console.log("hola");
+// Ejercicio autos de carrera..
+let tiempoPrimeraVuelta: number = Number(prompt("ingrese el tiempo de la primera vuelta"));
+let tiempoSedundaVuelta: number = Number(prompt("ingrese el tiempo de la segunda vuelta"));
+let tiempoTercerVuelta: number = Number(prompt("ingrese el tiempo de la tercer vuelta"));
+let tiempoCuartaVuelta: number = Number(prompt("ingrese el tiempo de la cuarta vuelta"));
+// Aqui se eingresan los tiempos de las vueltas...
+let tiempoTotal: number = tiempoPrimeraVuelta + tiempoSedundaVuelta + tiempoTercerVuelta + tiempoCuartaVuelta;
+// Suma de los tiempos de las vueltas
+let tiempoPromedioTotal: number = tiempoTotal / 4;
+// Promedio de tiempos de las vueltas
+
+
+console.log("El tiempo total es: ", tiempoTotal, "segundos");
+console.log("El tiempo promedio de vuelta es: ", tiempoPromedioTotal, "segundos");
+
+//Realizado por Dario Lopez - Comision A TDF - 24/04/2022
