b3d29cdeecc9b6e27fe4ab144910e9dfcf72ab85
Dario-Lopez-TDF <104174213+Dario-Lopez-TDF@users.noreply.github.com>
2022-04-24 18:14:07 -0300

    Segundo Ejercicio

--- src/ejercicio2.ts
+++ src/ejercicio2.ts
@@ -1,10 +1,10 @@
 // Ejercicio autos de carrera..
-let tiempoPrimeraVuelta: number = Number(prompt("ingrese el tiempo de la primera vuelta"));
+let tiempoPrimerVuelta: number = Number(prompt("ingrese el tiempo de la primer vuelta"));
 let tiempoSedundaVuelta: number = Number(prompt("ingrese el tiempo de la segunda vuelta"));
 let tiempoTercerVuelta: number = Number(prompt("ingrese el tiempo de la tercer vuelta"));
 let tiempoCuartaVuelta: number = Number(prompt("ingrese el tiempo de la cuarta vuelta"));
-// Aqui se eingresan los tiempos de las vueltas...
-let tiempoTotal: number = tiempoPrimeraVuelta + tiempoSedundaVuelta + tiempoTercerVuelta + tiempoCuartaVuelta;
+// Aqui se ingresan los tiempos de las vueltas...
+let tiempoTotal: number = tiempoPrimerVuelta + tiempoSedundaVuelta + tiempoTercerVuelta + tiempoCuartaVuelta;
 // Suma de los tiempos de las vueltas
 let tiempoPromedioTotal: number = tiempoTotal / 4;
 // Promedio de tiempos de las vueltas
