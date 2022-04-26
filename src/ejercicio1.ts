e625fb2bb0469befee813c3358fc69f5fa5a1d6b
Dario-Lopez-TDF <104174213+Dario-Lopez-TDF@users.noreply.github.com>
2022-04-26 19:29:01 -0300

    Martes 26 de Abril Arregle de comentarios en ejercicio 1

--- src/ejercicio1.ts
+++ src/ejercicio1.ts
@@ -1,12 +1,15 @@
+//Realizado por Dario Lopez - Comision A TDF - 23/04/2022
 //Ejercicio: cálculo de descuento..
+
+// Ingrese el precio del producto
 let precioInicialProducto: number = Number(prompt("ingrese el precio"));
-//Precio inicial del producto
-let descuentoDiezPorciento: number = 0.1;
 //Descuento del 10%
-let descuento: number = precioInicialProducto * descuentoDiezPorciento;
+let descuentoDiezPorciento: number = 0.1;
 //esto es el importe del descuento
+let descuento: number = precioInicialProducto * descuentoDiezPorciento;
+// resultado con descuento
 let resultado: number = precioInicialProducto - descuento;
 //...muestra el precio final del producto...
 console.log("el precio final es: ", resultado);
 
-//Realizado por Dario Lopez - Comision A TDF - 23/04/2022
+
