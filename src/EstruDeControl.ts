3c3bdc4a69888aced41b6ac1112c1886fca0ba90
Dario-Lopez-TDF <104174213+Dario-Lopez-TDF@users.noreply.github.com>
2022-05-01 11:14:15 -0300

    Ejercicio 3 - Suma entre Números Ejercicio 3 - Estructuras de Control - Suma entre Números Ejercicio entregable: clase 29/04/2022 hasta 02/05/2022

--- src/EstruDeControl.ts
+++ src/EstruDeControl.ts
@@ -18,16 +18,26 @@ if (continuar === "y") {
 }
 */
 
-/////Multiplos de 2 y 3
+/////Multiplos de 2 y 3 (% 2 == 0  y % 3 == 0) El resto da 0...
 // Algoritmo múltiplo
 
 let indice: number;
-let cadena: string = ""; //Es el contador??
-for (let indice = 1; indice < 10; indice++) {
-  if (indice % 2 == 0 || indice % 3 == 0) {
+let cadena: string = "";
+for (let indice: number = 1; indice <= 100; indice++) {
+  if (indice % 2 === 0 || indice % 3 === 0) {
     cadena = cadena + indice + "";
     console.log(cadena);
   }
 }
 
+/*Algoritmo Múltiplos
+let indice : number;
+let cadena : string = "";
+for (let indice : number = 1 ; indice <= 100 ; indice++ ) {
+if (indice % 2 == 0 || indice % 3 == 0) {
+cadena = cadena + indice + " ";
+}
+}
+console.log(cadena);
+*/
 //TAREA:  EJERCICIO ENTRGLABLE Y COMPARTIR LA DIRECCION DEL ARCHIVO DE GITHUB
---
+++ src/ejercicio3.ts
@@ -0,0 +1,36 @@
+/*Ejercicio 3 - Estructuras de Control - Suma entre Números
+Ejercicio entregable: clase 29/04/2022 
+
+Realizado por Dario Lopez - Comision A TDF - 01/05/2022
+
+Escriba un programa que pida al usuario dos números
+enteros, y luego retorne la suma de todos los números
+que están entre ellos
+• Por ejemplo, si los números son 2 y 7, debe entregar
+como resultado 2 + 3 + 4 + 5 + 6 + 7 = 27
+
+¿Qué ocurre si se ingresan los números al revés?
+Por ejemplo:
+en vez de 1 y 7 se ingresa 7 y 1
+en vez de 3 y 9 se ingresa 9 y 3
+¿Qué estrategia se puede usar para arreglar este problema?
+Recuerden los ejercicios que hicimos antes!
+*/
+
+let numeroA: number = Number(prompt("Ingrese primer número"));
+let numeroB: number = Number(prompt("Ingrese segundo número"));
+/*Se agrega una nueva variable "invertir" por si se ingresa primero
+un numero mayor y luego un menor */
+let contador, suma, invertir: number;
+contador = numeroA;
+if (numeroA > numeroB) {
+  contador = numeroA; // si B > A intercambio el primero por el segundo.
+  numeroA = numeroB;
+  numeroB = contador;
+}
+suma = 0;
+for (let contador: number = numeroA; contador <= numeroB; contador++) {
+  suma = suma + contador;
+}
+console.log("La suma de todos los números es:", suma);
+//End
